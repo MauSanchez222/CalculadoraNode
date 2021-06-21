8af7ed1bbf6b9496dda9ff29eb7154e4ea930ff2
MauSanchez222 <mauricioasan96@gmail.com>
2021-06-21 20:00:58 -0300

    segundo commit

---
+++ App/Calculadora.js
@@ -0,0 +1,29 @@
+'use strict';
+
+function suma(a,b) {
+    return a+b;
+}
+
+function resta(a,b) {
+    return a-b;
+}
+
+function multiplicar(a,b){
+    return a*b;
+}
+
+function dividir(a,b){
+
+if (b === 0) {
+    return new Error ("No se puede dividir por 0");
+}  else {
+    return a/b;
+}
+}
+
+module.exports = {
+    suma,
+    resta,
+    multiplicar,
+    dividir,
+}
